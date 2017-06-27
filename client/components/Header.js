92345689275963a23eb454bebec229949436132f
Haviar <peter.haviar@accenture.com>
2017-06-27 15:45:31 +0200

    using activeStyle prop on NavLink to change color of active link

--- client/components/Header.js
+++ client/components/Header.js
@@ -7,13 +7,13 @@ const Header = () => (
     <nav className="navbar navbar-default">
       <div className="container-fluid">
         <div className="navbar-header">
-          <NavLink className="navbar-brand" to="/">React App</NavLink>
+          <NavLink className="navbar-brand" exact to="/" activeStyle={{background:'skyblue'}}>React App</NavLink>
         </div>
         <ul className="nav navbar-nav">
-          <li ><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/teachers">Teachers</NavLink></li>
-          <li><NavLink to="/courses">Courses</NavLink></li>
+          <li ><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/about" activeStyle={{background:'#00c4cc'}}>About</NavLink></li>
+          <li><NavLink to="/teachers" activeStyle={{background:'#00c4cc'}}>Teachers</NavLink></li>
+          <li><NavLink to="/courses" activeStyle={{background:'#00c4cc'}}>Courses</NavLink></li>
         </ul>
       </div>
     </nav>
