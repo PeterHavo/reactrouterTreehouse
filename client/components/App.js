3dd938e000c64906336f75a7a6f8e8d0a96782ea
Haviar <peter.haviar@accenture.com>
2017-06-27 13:20:39 +0200

    Team treehouse tutorial, basic routing app

---
+++ client/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Router } from 'react-router-dom';
+//import {browserHistory} from 'react-router'; 
+
+import Home from './Home';
+import About from './About';
+import Header from './Header';
+
+class App extends React.Component{
+  constructor(props){
+      super(props);
+  }
+   render () {
+    return (< BrowserRouter>
+   
+    <div className="container">
+            <Header/>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+       
+   </div>
+        </BrowserRouter>
+      
+        )
+   }
+    
+  
+        
+}
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,16 @@
+
+
+/*
+    ./client/index.js
+    which is the webpack entry file
+*/
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render((
+    <App />
+), document.getElementById('root'))
