69961684e5c2c22c314e5f332be64ddedee45796
Haviar <peter.haviar@accenture.com>
2017-07-25 15:14:54 +0200

    adjust style

--- dist/index_bundle.js
+++ dist/index_bundle.js
@@ -26085,7 +26085,7 @@ var Header = function Header() {
           null,
           _react2.default.createElement(
             _reactRouterDom.NavLink,
-            { to: '/courses', activeStyle: { background: '#F45B69' }, id: 'test' },
+            { to: '/courses', activeStyle: { background: '#E4FDE1' }, id: 'test' },
             'Courses'
           )
         )
