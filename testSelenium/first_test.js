4ad23e8acb791fd68e7778f340efc7c1cb7b8af4
Haviar <peter.haviar@accenture.com>
2017-07-25 14:26:26 +0200

    testing

--- testSelenium/first_test.js
+++ testSelenium/first_test.js
@@ -6,6 +6,20 @@ var driver = new webdriver.Builder().
    build();
  
 driver.get('http://34.253.189.52');
-driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
-driver.findElement(webdriver.By.name('btnG')).click();
-driver.quit();
+driver.findElement(webdriver.By.id('test')).click();
+// driver.findElement(webdriver.By.name('btnG')).click();
+driver.wait(checkTitle, 500);
+
+function checkTitle () {
+    var promise = driver.getCurrentUrl().then((url) => {
+        if (url == 'http://34.253.189.2/courses/html') {
+            console.log('success');
+            return true;
+        } else 
+        {
+            console.log(`fail-----${url}`);
+        }
+    });
+    driver.quit();
+    return promise;
+}
